9064f81219afc078929b0d51b8c20d2ce13526c3
Stas Zarudensky <szarudensky@gmail.com>
2018-03-24 13:41:32 +0200

    test

--- js/showText.js
+++ js/showText.js
@@ -1,3 +1,4 @@
+window.addEventListener("load", function() { window. scrollTo(0, 0); });
 document.addEventListener("touchmove", function(e) { e.preventDefault() });
 $(window).ready(function(){
 	setTimeout ("$('#welcome').show('slow');",2000);
